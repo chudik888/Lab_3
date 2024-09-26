d636d2ef206860a370dfdd8a278c8a88d10ff1d4
chudik888 <166250551+chudik888@users.noreply.github.com>
2024-09-26 15:41:54 +0300

    Add files via upload

---
+++ PythonLab/lab3/lab3_0.py
@@ -0,0 +1,13 @@
+def reverse_exercise(list_: list[int]) -> None:
+    n = len(list_)
+    for i in range(n // 2):
+        list_[i], list_[n - 1 - i] = list_[n - 1 - i], list_[i]
+
+# Приклад використання
+list_ = list(map(int, input("Введіть числа через пробіл: ").split()))
+reverse_exercise(list_)
+'''
+# Перевірка
+assert list_ == [2, 10, 5, 1]'''
+
+print("Перевернутий список:", list_)
